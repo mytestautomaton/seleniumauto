4d69fcb47b535c2138ff9de4b1e032c05e861717
mytestautomaton <testautomation307@gmail.com>
2017-07-20 01:10:53 +0530

    Update NewTest.java

--- src/com/test/NewTest.java
+++ src/com/test/NewTest.java
@@ -4,7 +4,7 @@ public class NewTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- System.out.println("My self");
+ System.out.println("My self Mr.Ramesh");
 	}
 
 }
